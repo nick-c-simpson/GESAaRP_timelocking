e0ba4a07d844d9b6533d00be67d91826c5e01361
nick-c-simpson <nicholas.simpson.23@ucl.ac.uk>
2026-09-21 15:39:20 +0100

    Update GenerateWidFiles.R As 2afc task data was too large

--- GenerateWidFiles.R
+++ GenerateWidFiles.R
@@ -12,14 +12,12 @@ write_json(tafc_trials$si, "WidFiles/Exp2.json", auto_unbox = TRUE)
 
 # Load the 2AFCTasks
 
-rep_trials <- read.csv('RealData/reproduction_tasks.csv') %>%
-  filter(experiment_name == 'Replication') %>%
+rep_trials <- read.csv('RealData/reproduction_replication.csv') %>%
   reframe(si = unique(subject_identifier))
 
 write_json(rep_trials$si, "WidFiles/Exp3Rep.json", auto_unbox = TRUE)
 
-rep_trials <- read.csv('RealData/reproduction_tasks.csv') %>%
-  filter(experiment_name == 'Second Replication') %>%
+rep_trials <- read.csv('RealData/reproduction_second_replication.csv') %>%
   reframe(si = unique(subject_identifier))
 
 write_json(rep_trials$si, "WidFiles/Exp3SecRep.json", auto_unbox = TRUE)
